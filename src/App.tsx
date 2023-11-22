6bc6f1acd9af58526e96f7d2a94c1df46d5e20d5
Patrick White <patrickwhite.swe@gmail.com>
2023-11-22 14:45:09 -0700

    axios GET method to retrieve todos working only on startup, need to figure out how to refresh when adding a new todo. Need to add func to delete todos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Card from './Card.tsx'
 import InputRow from './InputRow.tsx';
+import axios from 'axios';
 
 function App() {
+  const [todoList, settodoList] = useState([]);
 
-  var title="exmp title";
-  var description="exmp desc";
-  var date="exmp date";
+  useEffect(() => {
+    axios.get('http://localhost:5154/allToDo')
+      .then(response => {
+        settodoList(response.data);
+      })
+
+  }, [])
 
   return (
     <>
       <div className="backplate">
         <InputRow />
         <div className="mainplate">
-          <Card 
-            title={title}
-            description={description}
-            date={date}
+          {todoList.map((item, index) => (
+            <Card
+            key={index}
+            title={item.id}
+            description={item.content}
+            isComplete={item.isComplete}
           />
+          ))}      
         </div>
       </div>
     </>
--- src/Card.tsx
+++ src/Card.tsx
@@ -3,19 +3,19 @@ import './Card.css'
 interface CardProps {
   title: String,
   description: String,
-  date: String
+  isComplete: String
 }
 const Card: React.FC<CardProps> = ({
   title,
   description,
-  date
+  isComplete
 }) => {
   return (
     <>
       <div className='card'>
         <div className='title'>{title}</div>
         <div className='description'>{description}</div>
-        <div className='date'>{date}</div>
+        <div className='isComplete'>{isComplete}</div>
       </div>
     </>
   )
